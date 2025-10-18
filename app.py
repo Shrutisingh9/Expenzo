c8d4ad01cb6fdf22813eb2808d312356771806d5
Shrutisingh9 <tanukum.ss784@gmail.com>
2025-10-18 12:03:34 +0530

    modify

--- app.py
+++ app.py
@@ -91,5 +91,9 @@ def logout():
     session.clear()
     return redirect(url_for('index'))
 
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
